7521b329a0dde622000371f70015f44ed8c67060
Rahmat Mozafari <38719925+Mozafari1@users.noreply.github.com>
2024-06-06 14:24:45 +0200

    Update Web.tsx

--- src/root/components/pages/price/Web.tsx
+++ src/root/components/pages/price/Web.tsx
@@ -22,11 +22,9 @@ const PriceCard: React.FC<IProps> = ({ basis, standard }) => {
           <li>{basis.pointD}</li>
           <li>{basis.pointE}</li>
         </ul>
-        <button className="banner__cta"onClick={()=>{
-   window.location.assign(
-                                                    "/contact-us"
-                                                )
-    }>
+      <button className="banner__cta" onClick={()=>{
+            window.location.assign("/contact-us")
+             }}>
           <a href="/contact-us">
             Kontakt for Basis
             <i className="fa-solid fa-arrow-right icons-right" />
@@ -83,11 +81,9 @@ const PriceCard: React.FC<IProps> = ({ basis, standard }) => {
           <li>Personlig konsultasjon for å sikre at alle behov møtes</li>
           <li>Tilgang til kontinuerlig teknisk støtte og vedlikehold</li>
         </ul>
-        <button className="banner__cta"onClick={()=>{
-   window.location.assign(
-                                                    "/contact-us"
-                                                )
-    }>
+      <button className="banner__cta" onClick={()=>{
+            window.location.assign("/contact-us")
+             }}>
           <a href="/contact-us">
             Kontakt for Standard
             <i className="fa-solid fa-arrow-right icons-right" />
